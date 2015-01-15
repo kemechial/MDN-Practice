63c28eacbfbe3f97fedff04a7ddd0195e3976b99
Kaan <Kaan@DeveloperK>
2015-01-15 18:13:43 +0200

    new file created...

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,10 @@
  * @author Ken
  */
 
+	var myHeading=document.querySelector('h1');
+	myHeading.innerHTML="Hello World!";
+
+	document.querySelector('html').onclick=function(){
+		alert('Stop poking me!');	
+	}
+
